15345146cfdeedbc28a8e9bb09e7ffea83f8a2a1
Chirag Singh <63168027+chiragrawat12@users.noreply.github.com>
2020-10-08 01:39:44 +0530

    Delete config.py

--- App/config.py
+++
@@ -1,8 +0,0 @@
-class Config:    
-    SECRET_KEY = '0e00190fdac7beb6ccaad48bc13e65b6'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS =True
-    MAIL_USERNAME ='chiragrawat12@gmail.com'
-    MAIL_PASSWORD = 'Chi1Rag2'
